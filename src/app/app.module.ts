8c78b10bd3cc1b93e3d08ccd07c926d19c7a63f3
Kate Power <kpow@shaw.ca>
2022-06-08 10:05:50 -0600

    starting project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { FormsModule } from '@angular/forms';
-import { PracticeComponent } from './practice/practice.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    PracticeComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
